2574f56833ee63e3b95cdb226273957b64e7bb2a
EngineerSmith <pasmith.bus@gmail.com>
2022-02-26 23:02:12 +0000

    Basics of map editor, needs graphics now

--- main.lua
+++ main.lua
@@ -4,20 +4,61 @@ local w, h = lg.getDimensions()
 local x, y = w/2, h/2
 local s = 1
 
-local hightSelected = 0
+local numSelected = 0
+
+local tool = 1
+local tools = {
+  [1] = "height",
+  [2] = "texture",
+  [3] = "walkable",
+} 
 
 local grid = require("grid").new(32,32)
 
 local map = {}
 
-local px, py = 0,0
 local click = function(x, y)
-  px, py = x, y
+  local gx, gy = grid:positionToTile(x, y)
+  if not map[gx] then
+    map[gx] = {}
+  end
+  if not map[gx][gy] then
+    map[gx][gy] = {}
+  end
+  local tile = map[gx][gy]
+  if tool == 1 or tool == 2 then
+    tile[tools[tool]] = numSelected
+  elseif tool == 3 then
+    tile[tools[tool]] = numSelected == 1
+  end
+end
+
+love.keypressed = function(key)
+  if key == "tab" then
+    tool = tool + 1
+    if tool > #tools then
+      tool = 1
+    end
+  elseif key == "c" then
+    if love.keyboard.isScancodeDown("rctrl", "lctrl") then
+      local txt = require("string.buffer").encode(map)
+      print("out:",tostring(txt))
+      love.system.setClipboardText(love.data.encode("string", "base64", txt))
+    end
+  elseif key == "v" then
+    if love.keyboard.isScancodeDown("rctrl", "lctrl") then
+      local txt = love.system.getClipboardText()
+      local txt = love.data.decode("string", "base64", txt)
+      print("in:",tostring(txt))
+      map = require("string.buffer").decode(txt)
+    end
+  elseif tonumber(key) then
+    numSelected = tonumber(key)
+  end
 end
 
 love.draw = function()
   grid:draw(x, y, w, h, s)
-  lg.circle("fill", px, py, 10)
 end
 
 local drag = false
@@ -40,7 +81,7 @@ end
 love.mousereleased = function(_,_, button)
   if button == 1 then
     drag = false
-    if love.timer.getTime() - clickTime < 0.12 then
+    if love.timer.getTime() - clickTime < 0.2 then
       click(clickx, clicky)
     end
   end
