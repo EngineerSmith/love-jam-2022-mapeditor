63cf7d1fb5c4fc25a6c89465f1426d84c6d2c70b
EngineerSmith <pasmith.bus@gmail.com>
2022-02-26 23:30:11 +0000

    Fixed issue with position to tile

--- main.lua
+++ main.lua
@@ -2,6 +2,7 @@ local lg = love.graphics
 
 local w, h = lg.getDimensions()
 local x, y = w/2, h/2
+print(x, y)
 local s = 1
 
 local numSelected = 0
@@ -17,8 +18,8 @@ local grid = require("grid").new(32,32)
 
 local map = {}
 
-local click = function(x, y)
-  local gx, gy = grid:positionToTile(x, y)
+local click = function(_x, _y)
+  local gx, gy = grid:positionToTile(_x-x, _y-y)
   if not map[gx] then
     map[gx] = {}
   end
@@ -33,6 +34,7 @@ local click = function(x, y)
   end
 end
 
+local showHeight = false
 love.keypressed = function(key)
   if key == "tab" then
     tool = tool + 1
@@ -54,11 +56,48 @@ love.keypressed = function(key)
     end
   elseif tonumber(key) then
     numSelected = tonumber(key)
+  elseif key == "rshift" or "lshift" then
+    showHeight = not showHeight
   end
 end
 
+local textures = {
+  [0] = {.9,.9,.9},
+  [1] = {.2,.5,.8}, -- set colours to match textures generally
+  [2] = {.1,.8,.4},
+  [3] = {.7,.6,.1},
+  [4] = {.5,.1,.7},
+  [5] = {.7,.7,.1},
+  [6] = {.1,.7,.7},
+  [7] = {.7,.1,.7},
+  [8] = {.9,.4,.6},
+  [9] = {.4,.1,.1},
+}
+
+local drawMap = function(x, y, s)
+  lg.push()
+  lg.translate(x, y)
+  for x, xtbl in pairs(map) do
+  for y, tile in pairs(xtbl) do
+    lg.push("all")
+    lg.translate(x * 32*s, y * 32*s)
+    local texture = tile.texture
+    if texture then
+      local c = textures[texture]
+      if c then lg.setColor(c) end
+    end
+    lg.rectangle("fill", 0,0, 32*s,32*s)
+    lg.pop()
+  end
+  end
+  lg.pop()
+end
+
+lg.setNewFont(20)
 love.draw = function()
+  drawMap(x, y, s)
   grid:draw(x, y, w, h, s)
+  lg.print(("Num: %d\nTool: %s"):format(numSelected, tools[tool]))
 end
 
 local drag = false
@@ -75,6 +114,7 @@ love.mousemoved = function(_,_, dx, dy)
   if drag then
     x = x + dx
     y = y + dy
+    print(x, y)
   end
 end
 
